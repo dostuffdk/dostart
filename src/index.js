dd8d49979916ec26853fa7b0914fde1072fd6c05
Anders Mogensen <anders@dostuff.dk>
2018-09-11 19:52:44 +0200

    A BEM css demo just to get started.

--- src/index.js
+++ src/index.js
@@ -4,3 +4,16 @@ const startNode = document.createElement('h1');
 startNode.innerHTML = 'Is this the real life? Is this just fantasy?';
 
 document.body.appendChild(startNode);
+
+const bemDemo = `
+<div class="something">
+    Blue
+    <p class="something__prop">Red</p>
+</div>
+
+<div class="something something--modifier">
+    Purple
+    <p class="something__prop something__prop--other-modifier">Green</p>
+</div>
+`;
+document.body.insertAdjacentHTML('beforeend', bemDemo);

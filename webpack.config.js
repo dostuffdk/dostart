c525d6e9265147e089b983d3f31e6456764e959c
Anders Mogensen <anders@dostuff.dk>
2018-09-11 18:02:30 +0200

    Make it possible to start devServer spcifying host and port.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    devServer: {
+        host: process.env.HOST, // use 0.0.0.0 to make it available on network
+        port: process.env.PORT,
+    },
     plugins: [
         new HtmlWebpackPlugin({
             title: 'doStart'

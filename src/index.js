3ed133e2d568a62200ce4587111541dd28f6f633
Anders Mogensen <anders@dostuff.dk>
2018-09-11 21:29:59 +0200

    Demoing images.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 import '@babel/polyfill';
 import './css/main.css';
 
+import lampImage from './images/lamps.png';
+import birdImage from './images/bird.png';
+
+// Title
 const startNode = document.createElement('h1');
 startNode.innerHTML = 'Is this the real life? Is this just fantasy?';
-
 document.body.appendChild(startNode);
 
+// An image
+const lampImg = document.createElement('img');
+lampImg.src = lampImage;
+document.body.appendChild(lampImg);
+
+// An image that is transformed to inline loading
+const birdImg = document.createElement('img');
+birdImg.src = birdImage;
+document.body.appendChild(birdImg);
+
+// Some BEM HTML that corresponds to BEM CSS in main.css
 const bemDemo = `
 <div class="something">
     Blue

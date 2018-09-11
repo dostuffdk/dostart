9fd2dcaef15f103c00fe649a54c971cbc0ca9a53
Anders Mogensen <anders@dostuff.dk>
2018-09-11 17:53:39 +0200

    We need some DOM!

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-console.log('doStart');
+const startNode = document.createElement('h1');
+startNode.innerHTML = 'Is this the real life? Is this just fantasy?';
+
+document.body.appendChild(startNode);
